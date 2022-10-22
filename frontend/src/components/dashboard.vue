000d69b2a1d13f5171e7849d273bc8dd9a0a7ec3
schoolassignedAG <aagarc35@cougarnet.uh.edu>
2022-10-22 13:39:06 -0500

    Dashboard Commit

--- frontend/src/components/dashboard.vue
+++ frontend/src/components/dashboard.vue
@@ -3,14 +3,41 @@
     <div>
       <h1 class="font-bold text-4xl text-red-700 tracking-widest text-center mt-10">Welcome</h1>
     </div>
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-x-6 gap-y-10">
+      <div class="flex flex-col col-span-2">
+        <table class="min-w-full shadow-md rounded">
+          <thead class="bg-gray-50 text-xl">
+            <tr>
+              <th class="p-4 text-left">Event Name</th>
+              <th class="p-4 text-left">Event Date</th>
+              <th class="p-4 text-left">Number Of Attendees</th>
+            </tr>
+          </thead>
+          <tbody class="divide-y divide-gray-300">
+            <tr v-for="event in eventData" :key="event._id">
+              <td class="p-2 text-left">{{ event.eventName }}</td>
+              <td class="p-2 text-left">{{ event.date }}</td>
+              <td class="p-2 text-left">{{ event.attendees }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </main>
 </template>
 <script>
+import axios from "axios";
 export default {
-  methods: {
-    routePush(routeName) {
-      this.$router.push({ name: routeName });
-    },
+  data() {
+    return {
+      eventData: []
+    };
   },
+  mounted() {
+    let apiURL = import.meta.env.VITE_ROOT_API + `/dashboard/getbydate`;
+    axios.get(apiURL).then((resp) => {
+      this.eventData = resp.data;
+    });
+  }
 };
 </script>
